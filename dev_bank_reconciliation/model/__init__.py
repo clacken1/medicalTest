bec41fd45cd16aa6fc5db97aad6a776798797de5
clacken1 <oneilclacken@yahoo.com>
2021-07-24 22:41:30 -0500

    Delete dev_bank_reconciliation directory

--- dev_bank_reconciliation/model/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import bank_reconciliation
-from . import account
--- dev_bank_reconciliation/report/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import reconcile_report
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
